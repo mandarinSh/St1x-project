86d1c7d18ead52801aa85136c9af63e1c9b4fe10
Alina Pereskokova <Alina.Pereskokova@emc.com>
2019-05-27 21:52:04 +0300

    simple test

--- client/ClientApp/e2e/tests/base-spec.js
+++ client/ClientApp/e2e/tests/base-spec.js
@@ -1,6 +1,17 @@
 describe('angularjs homepage todo list', function() {
-  it('should add a todo', function() {
+  // it('should add a todo', function() {
+  //   browser.get('http://localhost:4200/loginpage');
+  //   expect(browser.getTitle()).toEqual('ClientApp');
+  // });
+
+  it('should login', function() {
     browser.get('http://localhost:4200/loginpage');
-    expect(browser.getTitle()).toEqual('ClientApp');
+
+    element(by.model('nickname')).sendKeys('man1');
+    element(by.model('password')).sendKeys('111111');
+
+    element(by.id('log-in-button')).click();
+
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/dialogpage');
   });
 });
